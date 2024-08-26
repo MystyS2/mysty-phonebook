ce4c97d9d7a70a37db1b0b37ad8058ea9a5e0db3
MiHyeon <kkmmhh7478@gmail.com>
2024-08-26 15:07:48 +0900

    #2 add Spinner

--- src/component/Memo.js
+++ src/component/Memo.js
@@ -1,26 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Spinner } from "@nextui-org/react";
 
-const Memo = ({item}) => {
-    let memoImg = `https://github.com/MystyS2/mysty-phonebook/blob/master/public/assets/memo${item.index%6+1}.png?raw=true`
+const Memo = ({ item }) => {
+  let memoImg = `https://github.com/MystyS2/mysty-phonebook/blob/master/public/assets/memo${
+    (item.index % 6) + 1
+  }.png?raw=true`;
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const handleImageLoad = () => {
+    setIsImageLoaded(true);
+  };
   return (
-    <div className='w-[300px] relative'>
-        <div className='w-full align-middle'>
-            <img src={memoImg} alt='memo1'/>            
-        </div>
-        <div className='absolute top-[70px] left-16 text-lg text-center text-[#9b2a2a] font-medium'>
+    <div className="w-[300px] relative">
+      <div className="w-full align-middle">
+        <img src={memoImg} alt="memo1" onLoad={handleImageLoad} />
+      </div>
+      {isImageLoaded ? (
+        <div>
+          {" "}
+          <div className="absolute top-[70px] left-16 text-lg text-center text-[#9b2a2a] font-medium">
             Name
-        </div>
-        <div className='absolute top-[110px] left-16 text-lg text-center text-white'>
+          </div>
+          <div className="absolute top-[110px] left-16 text-lg text-center text-white">
             {item.name}
-        </div>
-        <div className='absolute top-[152px] left-16 text-lg text-center text-[#9b2a2a] font-medium'>
+          </div>
+          <div className="absolute top-[152px] left-16 text-lg text-center text-[#9b2a2a] font-medium">
             Phone
-        </div>
-        <div className='absolute top-[195px] left-16 text-lg text-center text-white'>
+          </div>
+          <div className="absolute top-[195px] left-16 text-lg text-center text-white">
             {item.phoneNumber}
+          </div>
         </div>
+      ) : (
+        <Spinner label="Loading" color="danger" labelColor="danger" className="grid place-items-center m-28"/>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Memo
+export default Memo;
